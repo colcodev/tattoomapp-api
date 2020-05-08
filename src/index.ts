f16b5461b0f90edf4aa3d49d71342806e0de0e86
Mario Colque <colkito@hotmail.com>
2020-05-08 03:26:01 -0300

    added sample controllers

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 /**
  * Required External Modules
  */
-import * as dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { itemsRouter } from "./items/items.router";
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use("/items", itemsRouter);
 
 /**
  * Server Activation
