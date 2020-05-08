72d920c325194b1828565b530266da2f7978db25
COLCO <mario@hashdog.com>
2020-05-08 02:08:34 -0300

    added initial code

---
+++ src/items/item.interface.ts
@@ -0,0 +1,8 @@
+// src/items/item.interface.ts
+
+export interface Item {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+}
---
+++ src/items/items.router.ts
@@ -0,0 +1,28 @@
+/**
+ * Required External Modules and Interfaces
+ */
+
+import express, { Request, Respose } from 'express';
+import * as ItemService from './items.service';
+import { Item } from './item.interface';
+import { Items } from './items.interface';
+
+/**
+ * Router Definition
+ */
+
+export const itemsRouter = express.Router();
+
+/**
+ * Controller Definitions
+ */
+
+// GET items/
+
+// GET items/:id
+
+// POST items/
+
+// PUT items/
+
+// DELETE items/:id
